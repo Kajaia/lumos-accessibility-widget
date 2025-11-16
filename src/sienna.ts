0e20ac3434fbac71014d855877433b16d2dfa0e4
Kajaia <lashakajaia31@gmail.com>
2025-11-16 22:54:16 +0400

    Reset saved language to default

--- src/sienna.ts
+++ src/sienna.ts
@@ -1,28 +1,33 @@
 import runAccessibility from "@/views/menu/runAccessibility";
-import { renderWidget } from "@/views/widget/widget"
-
-import { 
-    userSettings,
-    getSavedUserSettings
-} from '@/globals/userSettings';
+import { renderWidget } from "@/views/widget/widget";
 
 import {
-    pluginConfig
-} from "./globals/pluginConfig";
+  userSettings,
+  getSavedUserSettings,
+  STORAGE_KEY,
+} from "@/globals/userSettings";
+
+import { pluginConfig } from "./globals/pluginConfig";
 import { changeLanguage } from "./i18n/changeLanguage";
+import { getDefaultLanguage } from "./i18n/getDefaultLanguage";
+import { saveStorageData } from "./storage";
 
-export default function sienna({
-    options
-}) {
-    const savedSettings = getSavedUserSettings();
+export default function sienna({ options }) {
+  const savedSettings = getSavedUserSettings();
 
-    Object.assign(pluginConfig, options);
-    Object.assign(userSettings, savedSettings);
-    
-    runAccessibility();
-    renderWidget();
+  // Reset saved language to default
+  if (savedSettings) {
+    savedSettings.lang = getDefaultLanguage();
+    saveStorageData(STORAGE_KEY, savedSettings);
+  }
 
-    return {
-        changeLanguage
-    }
+  Object.assign(pluginConfig, options);
+  Object.assign(userSettings, savedSettings);
+
+  runAccessibility();
+  renderWidget();
+
+  return {
+    changeLanguage,
+  };
 }
